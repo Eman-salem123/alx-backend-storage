aa2df81a34b625aaee2998810a72d092de507e9d
Eman-salem123 <emanzatonaa@gmail.com>
2024-10-29 15:26:25 +0300

    lol

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -30,8 +30,10 @@ def data_cacher(method: Callable) -> Callable:
     return invoker
 
 
+@data_cacher
 def get_page(url: str) -> str:
     '''Returns the content of a URL after caching the request's response,
     and tracking the request.
     '''
     return requests.get(url).text
+  
